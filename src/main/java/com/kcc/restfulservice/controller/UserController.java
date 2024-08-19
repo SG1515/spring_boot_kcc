4f29e3cad203e35f54a02c30a6f382e1281b282e
Seong_gu <sunggu507@naver.com>
2024-08-19 11:34:26 +0900

    feat : user정보 출력

---
+++ src/main/java/com/kcc/restfulservice/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.kcc.restfulservice.controller;
+
+import com.kcc.restfulservice.UserDaoService;
+import com.kcc.restfulservice.bean.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+
+    public UserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+}
