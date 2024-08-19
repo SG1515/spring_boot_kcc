7f08b1008cb2ecb528c441af6a890e20e7cb23ee
Seong_gu <sunggu507@naver.com>
2024-08-19 14:10:42 +0900

    refector : 500 error가 아닌 404 error로 바꾸기

--- src/main/java/com/kcc/restfulservice/exception/UserNotFoundException.java
+++ src/main/java/com/kcc/restfulservice/exception/UserNotFoundException.java
@@ -1,12 +1,17 @@
 package com.kcc.restfulservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 
     //2xx -> ok
     //3xx -> redirect
     //4xx -> client
     //500 -> server
-
     public UserNotFoundException(String message) {
         super(message);
     }
