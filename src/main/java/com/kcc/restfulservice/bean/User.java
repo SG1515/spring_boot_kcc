71048fad148022614b32bfd5e1dcffc4fd9ad441
Seong_gu <sunggu507@naver.com>
2024-08-19 16:10:58 +0900

    refector : 클래스에서 민감데이터 처리하기

--- src/main/java/com/kcc/restfulservice/bean/User.java
+++ src/main/java/com/kcc/restfulservice/bean/User.java
@@ -1,6 +1,7 @@
 package com.kcc.restfulservice.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(value = {"password", "ssn"})
 public class User {
     private Integer id;
     @Size(min = 2, message = "name은 2글자 이상 입력해주세요.")
@@ -17,8 +19,8 @@ public class User {
     @Past(message = "등록일은 미래 날짜를 입력하실 수 없습니다.")
     private Date joinDate;
 
-    @JsonIgnore
+//    @JsonIgnore
     private String password;
-    @JsonIgnore
+//    @JsonIgnore
     private String ssn;
 }
