8aaf992af509f1526692cb969e114e3390876d12
Seong_gu <sunggu507@naver.com>
2024-08-19 10:54:10 +0900

    feat : setting

---
+++ src/main/java/com/kcc/restfulservice/RestfulserviceApplication.java
@@ -0,0 +1,14 @@
+package com.kcc.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulserviceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("KCC SpringBoot!!!");
+		SpringApplication.run(RestfulserviceApplication.class, args);
+	}
+
+}
