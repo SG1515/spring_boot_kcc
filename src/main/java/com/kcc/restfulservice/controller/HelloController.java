cab0e7ff842364799f15178a09f9722eea5cc741
Seong_gu <sunggu507@naver.com>
2024-08-19 11:14:25 +0900

    feat : pathvariable test

--- src/main/java/com/kcc/restfulservice/controller/HelloController.java
+++ src/main/java/com/kcc/restfulservice/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.kcc.restfulservice.controller;
 
 import com.kcc.restfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,11 @@ public class HelloController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean!!");
+    }
+
+    @GetMapping("/hello-world-bean/{name}")
+    public HelloWorldBean helloWorldBean2(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello world bean, %s", name));
     }
 
 }
