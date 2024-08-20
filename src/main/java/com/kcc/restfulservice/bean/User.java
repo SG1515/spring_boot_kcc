2d481cf5f54ef8f17cb2c709085fff87c1f8c38a
Seong_gu <sunggu507@naver.com>
2024-08-20 10:47:40 +0900

    feat : h2 database 활용하기. 기본데이터생성

--- src/main/java/com/kcc/restfulservice/bean/User.java
+++ src/main/java/com/kcc/restfulservice/bean/User.java
@@ -3,19 +3,30 @@ package com.kcc.restfulservice.bean;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Entity
+@Table(name ="users")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
+
 
     @Schema(title = "사용자 이름", description = "사용자 이름을 입력하세요.")
     @Size(min = 2, message = "name은 2글자 이상 입력해주세요.")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, name, join_date, password, ssn) values (9001,'kim', now(), 'pass1', '708080-1122331')
+insert into users(id, name, join_date, password, ssn) values (9002,'park', now(), 'pass1', '108080-1122331')
+insert into users(id, name, join_date, password, ssn) values (9003,'min', now(), 'pass1', '508080-1122331')
